b21c514913076404079159a0cd859cd1d22165af
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-07-17 22:19:23 +0400

    feat: minor changes

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,5 +1,5 @@
 import { IPiece } from "../models/Piece";
 
 export default function Collected({ value }: { value: IPiece }) {
-  return <button className="collected"> {value.icon} </button>;
+  return <span className="collected"> {value.icon} </span>;
 }
